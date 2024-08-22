94de23e593a930d3e4fc962ac35cfb1a88fa25a1
y_kawanishi <yat@yat-net.com>
2024-08-22 14:37:37 +0900

    record edit support

--- address.js
+++ address.js
@@ -1,20 +1,28 @@
 ( () => {
 	'use strict';
 	kintone.events.on( 'app.record.create.change.zipcode', ( event ) => {
-		const record = event.record;
-		const zipcode = record.zipcode.value;
-
-		$.ajax ( {
-			url: 'https://api.zipaddress.net/?zipcode=' + zipcode,
-			datatype: 'jsonp',
-			async: false,
-			success: ( response ) => {
-				record.fullAddress.value = response.data.fullAddress;
-			},
-			error: ( response ) => {
-				alert( '郵便番号からの住所検索に失敗しました' );
-			}
-		} );
+		loadaddress( event );
+		return event;
+	} );
+	kintone.events.on( 'app.record.edit.change.zipcode', ( event ) => {
+		loadaddress( event );
 		return event;
 	} );
 } )();
+
+const loadaddress = ( event ) => {
+	const record = event.record;
+	const zipcode = record.zipcode.value;
+
+	$.ajax ( {
+		url: 'https://api.zipaddress.net/?zipcode=' + zipcode,
+		datatype: 'jsonp',
+		async: false,
+		success: ( response ) => {
+			record.fullAddress.value = response.data.fullAddress;
+		},
+		error: ( response ) => {
+			alert( '郵便番号からの住所検索に失敗しました' );
+		}
+	} );
+}
